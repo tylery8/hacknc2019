c1eaee4ed2c515aeeb1e84e9c27bb02bdfeeb903
tylery8 <tylertfc99@gmail.com>
2019-10-12 18:50:31 -0400

    Hehehe i did a thing

---
+++ Child.ts
@@ -0,0 +1,108 @@
+class Child {
+
+    private parent: Parent;
+    private funds: Fund[];
+    private banned: string[];
+    private approved: string[];
+
+    constructor(parent: Parent) {
+        this.parent = parent;
+        this.funds = [];
+        this.banned = [];
+        this.approved = [];
+
+        this.parent.addChild(this);
+    }
+
+    request(amount: number, stores: string[]) {
+        this.parent.receiveRequest(new Requests(this, new Fund(amount, stores)));
+    }
+
+    receiveFund(fund: Fund) {
+        this.funds[this.funds.length] = fund;
+    }
+
+    getBanned() {
+      return this.banned;
+    }
+
+    getApproved() {
+      return this.approved;
+    }
+
+    getTotalAmount() {
+        let total: number = 0;
+        for (let i = 0; i < this.funds.length; i++) {
+            total += this.funds[i].getAmount();
+        }
+        return total;
+    }
+
+    getAllExpenses() {
+        let all: Expense[] = [];
+        for (let i = 0; i < this.funds.length; i++) {
+            all.concat(this.funds[i].getExpenses());
+        }
+        return all;
+    }
+
+    addBanned(store: string) {
+        this.removeApproved(store);
+        this.removeBanned(store);
+        this.banned[this.banned.length] = store;
+    }
+
+    addApproved(store: string) {
+        this.removeApproved(store);
+        this.removeBanned(store);
+        this.approved[this.approved.length] = store;
+    }
+
+    removeBanned(store: string) {
+        for (let i = 0; i < this.banned.length; i++) {
+            if (this.banned[i] === store) {
+                this.banned.splice(i,1);
+            }
+        }
+    }
+
+    removeApproved(store: string) {
+        for (let i = 0; i < this.approved.length; i++) {
+            if (this.approved[i] === store) {
+                this.approved.splice(i,1);
+            }
+        }
+    }
+
+    pay(expense: Expense) {
+        let best_Fund: Fund[] = [];
+        
+        for (let i = 0; i < this.funds.length; i++) {
+            if (this.acceptableFund(this.funds[i], expense)) {
+                if (best_Fund.length === 0) {
+                    best_Fund[0] = this.funds[i];
+                }
+                else if (this.funds[i].getStores().length < best_Fund[0].getStores().length) {
+                    best_Fund[0] = this.funds[i];
+                }
+                else if (this.funds[i].getStores().length === best_Fund[0].getStores().length && this.funds[i].getAmount() >= best_Fund[0].getAmount()){
+                    best_Fund[0] = this.funds[i];
+                }
+            }
+        }
+
+        if (best_Fund.length === 0) {
+            return false;
+        }
+
+        best_Fund[0].addExpense(expense);
+        return true;
+    }
+
+    acceptableFund(fund: Fund, expense: Expense) {
+        if (fund.getStores().length === 0) {
+            return fund.getAmount() >= expense.getAmount() && this.banned.indexOf(expense.getStore()) === -1;
+        }
+        return fund.getAmount() >= expense.getAmount() && fund.getStores().indexOf(expense.getStore()) >= 0;
+    }
+}
---
+++ Parent.ts
@@ -0,0 +1,47 @@
+class Parent {
+
+    private children: Child[];
+    private requests: Requests[];
+
+    constructor() {
+        this.children = [];
+        this.requests = [];
+    }
+
+    getChildren() {
+        return this.children;
+    }
+
+    getRequests() {
+        return this.requests;
+    }
+
+    addChild(child: Child) {
+        this.children[this.children.length] = child;
+    }
+
+    removeChild(child: Child) {
+        for (let i = 0; i < this.children.length; i++) {
+            if (this.children[i] === child) {
+                this.children.splice(i,1);
+            }
+        }
+    }
+
+    sendFund(child: Child, fund: Fund) {
+        child.receiveFund(fund);
+    }
+
+    receiveRequest(request: Requests) {
+        this.requests[this.requests.length] = request;
+    }
+
+    approveRequest(index: number) {
+        let request: Requests = this.requests[index];
+        request.getChild().receiveFund(request.getFund());
+    }
+
+    denyRequest(index: number) {
+        this.requests.splice(index,1);
+    }
+}
