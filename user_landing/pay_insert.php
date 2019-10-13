005ad9863575a3d4b1aa71edd538b7dba50c7721
Sahil Patel <sahilp@live.unc.edu>
2019-10-13 01:24:20 -0400

    overdraft

--- user_landing/pay_insert.php
+++ user_landing/pay_insert.php
@@ -16,83 +16,112 @@ $store = $_POST['store'];
 $banned_stores = $dependent_row['banned_stores'];
 $approved_stores = $dependent_row['approved_stores'];
 
-//for loop through each fund
-
 $conn = new mysqli('localhost', 'root', '', 'registration_storage');
 $sql ="SELECT * FROM fund WHERE dependent_u_name = '$_SESSION[username]' ";
 $result = mysqli_query($conn, $sql);
 $numRows = mysqli_num_rows($result);
 
-if (!strpos($banned_stores, $store)) {
+$totalpossible=0;
 
-    $best_id = -1;
-    $best_stores_length = 0;
-    $best_stores_amount = 0;
+for ($x = 0; $x < $numRows; $x++) {
+    
+    $fund_row = mysqli_fetch_array($result);
+    
+    if (strlen($fund_row['stores']) > 0) {
 
-    for ($x = 0; $x < $numRows; $x++) {
-        $fund_row = mysqli_fetch_array($result);
-        //you can now access elements on the row by using $fund_row['stores'] ['amount'];
+        if ((strpos($fund_row['stores'], $store) > 0)) {
 
-        if (strlen($fund_row['stores']) > 0) {
+            $totalpossible += $fund_row['amount'];
 
-            if ($amount <= $fund_row['amount'] && (strpos($fund_row['stores'], $store) > 0)) {
+        }
 
-                if ($best_id == -1) {
-                    $best_id = $fund_row['id'];
-                    $best_stores_length = substr_count($fund_row['stores'], '),(');
-                    $best_stores_amount = $fund_row['amount'];
-                } else if (substr_count($fund_row['stores'], '),(') < $best_stores_length) {
-                    $best_id = $fund_row['id'];
-                    $best_stores_length = substr_count($fund_row['stores'], '),(');
-                    $best_stores_amount = $fund_row['amount'];
-                } else if (substr_count($fund_row['stores'], '),(') == $best_stores_length && $fund_row['amount'] < $best_stores_amount) {
-                    $best_id = $fund_row['id'];
-                    $best_stores_amount = $fund_row['amount'];
+    } else {
+        $totalpossible += $fund_row['amount'];
+    }
+}
+//for loop through each fund
+
+while ($amount>0) {
+
+    $conn = new mysqli('localhost', 'root', '', 'registration_storage');
+    $sql ="SELECT * FROM fund WHERE dependent_u_name = '$_SESSION[username]' ";
+    $result = mysqli_query($conn, $sql);
+    $numRows = mysqli_num_rows($result);
+
+    if (!strpos($banned_stores, $store)) {
+
+        $best_id = -1;
+        $best_stores_length = 0;
+        $best_stores_amount = 0;
+
+        for ($x = 0; $x < $numRows; $x++) {
+            $fund_row = mysqli_fetch_array($result);
+            //you can now access elements on the row by using $fund_row['stores'] ['amount'];
+
+            if (strlen($fund_row['stores']) > 0) {
+
+                if ((strpos($fund_row['stores'], $store) > 0)) {
+
+                    if ($best_id == -1) {
+                        $best_id = $fund_row['id'];
+                        $best_stores_length = substr_count($fund_row['stores'], '),(');
+                        $best_stores_amount = $fund_row['amount'];
+                    } else if (substr_count($fund_row['stores'], '),(') < $best_stores_length) {
+                        $best_id = $fund_row['id'];
+                        $best_stores_length = substr_count($fund_row['stores'], '),(');
+                        $best_stores_amount = $fund_row['amount'];
+                    } else if (substr_count($fund_row['stores'], '),(') == $best_stores_length && $fund_row['amount'] < $best_stores_amount) {
+                        $best_id = $fund_row['id'];
+                        $best_stores_amount = $fund_row['amount'];
+                    }
+
                 }
 
-            }
-
-        } else {
-
-            if ($amount <= $fund_row['amount']) {
+            } else {
                 $best_id = $fund_row['id'];
             }
 
+            if ($best_id >= 0) {
+
+                $conn = new mysqli('localhost', 'root', '', 'registration_storage');
+                $sql ="SELECT * FROM fund WHERE id = '$best_id' ";
+                $result = mysqli_query($conn, $sql);
+                $numRows = mysqli_num_rows($result);
+        
+                $fund_row = mysqli_fetch_array($result);
+                if ($fund_row['amount']>=$amount){
+                    $new_amount = $fund_row['amount'] - $amount;
+                    $amount=0;
+                }
+                else {
+                    $amount = $amount -$fund_row['amount'];
+                    $new_amount = 0;
+                }
+                $sql ="UPDATE fund
+                        SET amount = '$new_amount'
+                        WHERE id = '$fund_row[id]' ";
+                $result = mysqli_query($conn, $sql);
+
+
+                $new_expense = $fund_row['expenses']."(".$store.","."$amount".")";
+
+                $sql ="UPDATE fund
+                        SET expenses = '$new_expense'
+                        WHERE id = '$fund_row[id]' ";
+                $result = mysqli_query($conn, $sql);
+
+                $approved = true;
+        
+            }
+
+
         }
-
-        if ($best_id >= 0) {
-
-            $conn = new mysqli('localhost', 'root', '', 'registration_storage');
-            $sql ="SELECT * FROM fund WHERE id = '$best_id' ";
-            $result = mysqli_query($conn, $sql);
-            $numRows = mysqli_num_rows($result);
-    
-            $fund_row = mysqli_fetch_array($result);
-            $new_amount = $fund_row['amount'] - $amount;
-
-            $sql ="UPDATE fund
-                    SET amount = '$new_amount'
-                    WHERE id = '$fund_row[id]' ";
-            $result = mysqli_query($conn, $sql);
-
-            $new_expense = $fund_row['expenses']."(".$store.","."$amount".")";
-
-            $sql ="UPDATE fund
-                    SET expenses = '$new_expense'
-                    WHERE id = '$fund_row[id]' ";
-            $result = mysqli_query($conn, $sql);
-
-            $approved = true;
-    
-        }
-
-
     }
-
+}
     $sql ="DELETE FROM fund WHERE amount = '0'";
     $result = mysqli_query($conn, $sql);
 
-}
+
 echo $approved;
 
 ?>
